3d2757fc589616ab8db7d3dacfb41fa1f34f843a
wesleyruchel <wesleyruchel@gmail.com>
2024-09-24 21:18:07 -0300

    Deploy 202409242118

---
+++ assets/QSpace.dbc53322.js
@@ -0,0 +1,1 @@
+import{h as e,t as a}from"./index.129f0960.js";const p=e("div",{class:"q-space"});var t=a({name:"QSpace",setup(){return()=>p}});export{t as Q};
---
+++ assets/routeHelpers.90713f5e.js
@@ -0,0 +1,1 @@
+import{V as e}from"./index.129f0960.js";function u(o){const t=e();return["perfil"].includes(o)?`/${o}`:`/${t.userAuthRolePrefix}/${o}`}export{u as g};
