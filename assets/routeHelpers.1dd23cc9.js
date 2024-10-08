a85d27a48c0d43a36c25b14e146cdc7d0789cdce
wesleyruchel <wesleyruchel@gmail.com>
2024-10-07 22:47:50 -0300

    Deploy 071020242245

---
+++ assets/routeHelpers.1dd23cc9.js
@@ -0,0 +1,1 @@
+import{X as e}from"./index.a1e82d74.js";function u(o){const t=e();return["perfil"].includes(o)?`/${o}`:`/${t.userAuthRolePrefix}/${o}`}export{u as g};
---
+++ assets/routeHelpers.4bee43a6.js
@@ -0,0 +1,1 @@
+import{X as e}from"./index.52d40142.js";function u(o){const t=e();return["perfil"].includes(o)?`/${o}`:`/${t.userAuthRolePrefix}/${o}`}export{u as g};
